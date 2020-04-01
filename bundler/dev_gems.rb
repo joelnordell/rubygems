9822cdbe3f92822cbe5289c0f3a9c6ef0a272fc5
David Rodríguez <deivid.rodriguez@riseup.net>
2020-04-01 23:24:30 +0200

    Avoid the rspec meta gem

--- bundler/dev_gems.rb
+++ bundler/dev_gems.rb
@@ -7,3 +7,5 @@ gem "parallel_tests", "~> 2.29"
 gem "rake", "~> 13.0"
 gem "ronn", "~> 0.7.3", :platform => :ruby
 gem "rspec-core", "~> 3.8"
+gem "rspec-expectations", "~> 3.8"
+gem "rspec-mocks", "~> 3.8"
